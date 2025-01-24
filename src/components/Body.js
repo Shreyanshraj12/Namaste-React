9120673ac93c8c73612339132f56f3bb465ac6b4
Shreyansh Raj <shreyanshgupt12@gmail.com>
2025-01-24 19:18:06 +0530

    adding shimmer ui and makin better functionality of search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,20 @@
 import { useState,useEffect } from "react";
 import { ResturantList } from "../constants";
 import  ResturantCard  from "./ResturantCard";
+import Shimmer from "./Shimmer";
 
 
-const filterData = (searchText, resturants) => {
-    return resturants.filter((resturant) =>
-      resturant.info.name.includes(searchText)
+const filterData = (searchText, allResturant) => {
+    return allResturant.filter((resturant) =>
+      resturant.info.name.toLowerCase().includes(searchText)
     );
   };
   
 
 
 const Body = ()=>{
-    const[resturants, setResturants] = useState(ResturantList);
+  const[allResturants, setAllResturants] = useState([]);
+    const[filteredResturants, setFilterResturants] = useState([]);
     const [searchText, setSearchText] = useState("");
    
 
@@ -26,28 +28,31 @@ const Body = ()=>{
     async function  getResturants(){
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.2585371&lng=82.06598579999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json();
-    setResturants(json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setAllResturants(json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilterResturants(json?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 
     }
-    return(
+   
+    return (allResturants.length === 0) ? <Shimmer/> :(
         <>
         <div className="search-container">
             <input type="text" className="search-input" placeholder="Search" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
             <button className="search-btn" onClick={()=>{
-            const data = filterData(searchText, resturants);
-            setResturants(data);
+            const data = filterData(searchText, allResturants);
+            setFilterResturants(data);
            
             }}>Search</button>
         </div>
 
-
-
-           
+ 
+         
         <div className="resturant-list">
+          {filteredResturants.length === 0 ? (<h1>Sorry No Iteams Found 😢</h1>) :( 
            
-           {resturants.map((p)=>{
-           return  <ResturantCard {...p.info} key={p.info.id} />
-           })}
+           filteredResturants.map((p)=>(
+             <ResturantCard {...p.info} key={p.info.id} />
+            ))
+          )}
             
         </div>
         </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 const Title = ()=>(
     <a href="/">
     <img className="logo" alt="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS51Fg8ECaMCb6T2yAd4CgUjy_hu7Zk1OKEZw&s"/>
     </a>
 )
 
+
+
 const HeaderComponent = ()=>{
+    const [loggedIn,  setloggedIn] = useState(true);
+    function toggle(){
+            setloggedIn(!loggedIn)
+        
+    }
     return(
         <div className="header">
             <Title/>
@@ -16,6 +24,8 @@ const HeaderComponent = ()=>{
                 <li>Cart</li>
                 </ul>
             </div>
+            {loggedIn ? (<button onClick={toggle}>LogOut</button>) : (<button onClick={toggle}>LogIn</button>)}
+            
         </div>
     );
 }
