d5fbed0f99c0e09ebc223d82c2e84d30f3eb8354
Shreyansh Raj <shreyanshgupt12@gmail.com>
2025-02-03 18:20:03 +0530

    improved the hide functionallity

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -5,7 +5,12 @@ const Section = ({title,description, isVisible, setIsVisible})=>{
     return(
         <div className="border border-black p-2 m-2">
             <h3 className="font-bold text-xl">{title}</h3>
-           {isVisible ? (<button onClick={()=> setIsVisible(false)} className="cursor-pointer underline">Hide</button>) : (<button onClick={()=> setIsVisible(true)} className="cursor-pointer underline">Show</button>)}
+        <button
+        onClick={() => setIsVisible(!isVisible)} 
+        className="cursor-pointer underline"
+      >
+        {isVisible ? "Hide" : "Show"}
+      </button>
            {isVisible && <p>{description}</p>}
         </div>
         
@@ -21,21 +26,21 @@ const Instamart = ()=>{
     })
     return <div>
         <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
-        <Section title={"About Instamart"} description={"Different cultures have their own unique cuisines, shaped by local ingredients and cooking techniques. For example, Indian cuisine is rich in spices and diverse flavors, while Italian food is known for its pasta, cheese, and olive oil. Chinese cuisine emphasizes balance with a mix of sweet, sour, salty, and spicy elements. Fast food, such as burgers and pizza, has gained global popularity due to convenience, but traditional dishes remain important in many societies."} isVisible={sectionConfig.showAbout} setIsVisible={()=> setSectionConfig({showAbout: true,
+        <Section title={"About Instamart"} description={"Different cultures have their own unique cuisines, shaped by local ingredients and cooking techniques. For example, Indian cuisine is rich in spices and diverse flavors, while Italian food is known for its pasta, cheese, and olive oil. Chinese cuisine emphasizes balance with a mix of sweet, sour, salty, and spicy elements. Fast food, such as burgers and pizza, has gained global popularity due to convenience, but traditional dishes remain important in many societies."} isVisible={sectionConfig.showAbout} setIsVisible={()=> setSectionConfig((prev)=>({showAbout: !prev.showAbout,
         showTeam: false,
         showCareers: false,
 
-        })}/>
-        <Section title={"Team Instamart"} description={"Different cultures have their own unique cuisines, shaped by local ingredients and cooking techniques. For example, Indian cuisine is rich in spices and diverse flavors, while Italian food is known for its pasta, cheese, and olive oil. Chinese cuisine emphasizes balance with a mix of sweet, sour, salty, and spicy elements. Fast food, such as burgers and pizza, has gained global popularity due to convenience, but traditional dishes remain important in many societies."} isVisible={sectionConfig.showTeam}  setIsVisible={()=> setSectionConfig({showAbout: false,
-        showTeam: true,
+        }))}/>
+        <Section title={"Team Instamart"} description={"Different cultures have their own unique cuisines, shaped by local ingredients and cooking techniques. For example, Indian cuisine is rich in spices and diverse flavors, while Italian food is known for its pasta, cheese, and olive oil. Chinese cuisine emphasizes balance with a mix of sweet, sour, salty, and spicy elements. Fast food, such as burgers and pizza, has gained global popularity due to convenience, but traditional dishes remain important in many societies."} isVisible={sectionConfig.showTeam}  setIsVisible={()=> setSectionConfig((prev)=>({showAbout: false,
+        showTeam: !prev.showTeam,
         showCareers: false,
 
-        })}/>    
-        <Section title={"Careers Instamart"} description={"Beyond sustenance, food has deep emotional and social connections. Festivals, celebrations, and family gatherings often revolve around special meals, reinforcing bonds and traditions. Street food is an integral part of many cultures, offering affordable and flavorful dishes."} isVisible={sectionConfig.showCareers}    setIsVisible={()=> setSectionConfig({showAbout: false,
+        }))}/>    
+        <Section title={"Careers Instamart"} description={"Beyond sustenance, food has deep emotional and social connections. Festivals, celebrations, and family gatherings often revolve around special meals, reinforcing bonds and traditions. Street food is an integral part of many cultures, offering affordable and flavorful dishes."} isVisible={sectionConfig.showCareers}    setIsVisible={()=> setSectionConfig((prev)=>({showAbout: false,
         showTeam: false,
-        showCareers: true,
+        showCareers: !prev.showCareers,
 
-        })}/>
+        }))}/>
     </div>
 }
 
